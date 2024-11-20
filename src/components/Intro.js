ceff39acdf06dc07f59ee5060f06b6e256f29b0c
Naveen Kumar <masternk007@gmail.com>
2024-11-20 14:49:14 +0530

    commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
-import Me from '../assets/Images/profile-img.png'
+import Me from '../assets/Images/profile_img.webp'
 
 
 const Box = styled(motion.div)`
